e8d058e2854e12dc26fbb58150e9641f6fd8d35e
lch <lch@192.168.0.168>
2017-11-01 22:45:40 +0800

    finish preprocessor

--- LongestPalindroma/src/com/lch920619x/LongestPalindroma/LongestPalindroma.java
+++ LongestPalindroma/src/com/lch920619x/LongestPalindroma/LongestPalindroma.java
@@ -5,6 +5,10 @@ public class LongestPalindroma {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String str=new String("abcdefg");
+		PreProcess p=new PreProcess();
+		str = p.preProcess(str);
+		System.out.println(str);
 	}
 
 }
--- LongestPalindroma/src/com/lch920619x/LongestPalindroma/PreProcess.java
+++ LongestPalindroma/src/com/lch920619x/LongestPalindroma/PreProcess.java
@@ -2,4 +2,14 @@ package com.lch920619x.LongestPalindroma;
 
 public class PreProcess {
 
+	public String preProcess(String str){
+		if(str.length()==0)
+			return new String("$$");
+		String newStr = new String("$#");
+		for (int i=0; i<str.length();i++) {
+			newStr=newStr+str.charAt(i)+"#";
+		}
+		newStr=newStr+"$";
+		return newStr;
+	}
 }
