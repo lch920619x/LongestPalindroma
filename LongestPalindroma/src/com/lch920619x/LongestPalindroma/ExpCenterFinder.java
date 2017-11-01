39e267ea81917f5292f8820d157f58def6a45fd7
lch <lch@192.168.0.168>
2017-11-01 23:27:48 +0800

    minor clean up

--- LongestPalindroma/src/com/lch920619x/LongestPalindroma/ExpCenterFinder.java
+++ LongestPalindroma/src/com/lch920619x/LongestPalindroma/ExpCenterFinder.java
@@ -4,17 +4,16 @@ package com.lch920619x.LongestPalindroma;
 public class ExpCenterFinder {
 	public String findPalindroma(String str) {
 		
-		int[] P=new int[str.length()-2];
-		P[2]=1;
+		int[] P=new int[str.length()];
 		int maxIndex = 0;
-		for (int i=3;i<str.length()-3;i++) {
+		for (int i=1;i<str.length()-1;i++) {
 			while (str.charAt(i-P[i]-1) == str.charAt(i+P[i]+1))
 			P[i]++;
 			
 			if (P[i]>P[maxIndex])
 				maxIndex=i;
 		}
-		
+
 		String palin = str.substring(maxIndex-P[maxIndex],maxIndex+P[maxIndex] );
 		return palin;
 		
--- LongestPalindroma/src/com/lch920619x/LongestPalindroma/ManacherFinder.java
+++ LongestPalindroma/src/com/lch920619x/LongestPalindroma/ManacherFinder.java
@@ -3,8 +3,18 @@ package com.lch920619x.LongestPalindroma;
 public class ManacherFinder {
 
 	public String findPalindroma(String str) {
+		int[] P=new int[str.length()];
+		int maxIndex = 0;
+		for (int i=1;i<str.length()-1;i++) {
+			while (str.charAt(i-P[i]-1) == str.charAt(i+P[i]+1))
+			P[i]++;
+			
+			if (P[i]>P[maxIndex])
+				maxIndex=i;
+		}
+
+		String palin = str.substring(maxIndex-P[maxIndex],maxIndex+P[maxIndex] );
+		return palin;
 		
-		
-		return str;
 	}
 }
